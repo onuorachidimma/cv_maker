830432dd2d2c4b698f2a9a7704d49a79c39a9aee
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-21 17:32:04 +0100

    intallations: install  dependencies

---
+++ elevate_cv/src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-3xl font-bold underline">
+          Hello, Vite and React with Tailwind CSS!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
