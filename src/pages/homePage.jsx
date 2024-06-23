556b34dd6ae27890d93936ddadc9bfe996ecac86
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-23 18:30:29 +0100

    edit: edit homepage content

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,7 +2,7 @@ const HomePage = () => {
     return(
         <>
             <div className="flex items-center justify-center min-h-screen">
-                <p className="text-3xl font-bold underline">I am the Home page</p>
+                <p className="text-3xl font-bold underline">I am the Home page, Welcome to Elevate Cv</p>
             </div>
         </>
     )
