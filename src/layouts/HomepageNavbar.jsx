6a404afdf629c00b60f25e591eb808dfbe879278
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-06-28 21:21:28 +0300

    Feat: Add Homepage Navbar

---
+++ src/layouts/HomepageNavbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function HomepageNavbar() {
+  return (
+    <>
+        <div id='navbar'> 
+            <img src="" alt="" />
+            <div>
+                <ul>
+                    <li>Resume Builder</li>
+                    <li>Tool</li>
+                    <li>More</li>
+                </ul>
+            </div>
+            <div>
+                <button>Login</button>
+                <button>Sign up</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default HomepageNavbar
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,6 +1,10 @@
+import HomepageNavbar from '../layouts/HomepageNavbar'
+
+
 const HomePage = () => {
     return(
         <>
+            <HomepageNavbar />
             <div className="flex items-center justify-center min-h-screen">
                 <p className="text-3xl font-bold underline">I am the Home page, Welcome to Elevate Cv</p>
             </div>
