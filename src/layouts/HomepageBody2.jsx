4aefe3605b39f61d63e0d0a909bac5ef3f3d1faa
khadijah <phunmilawal@gmail.com>
2024-06-28 14:35:24 +0100

    feat: added body and vite

---
+++ src/layouts/HomepageBody2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomepageBody2 = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="section"><h1>Section 1</h1></div>
+        <div className="section"><h1>Section 2</h1></div>
+        <div className="section">Section 3</div>
+      </div>
+    </>
+  );
+};
+
+export default HomepageBody2;
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,19 @@
+import React from 'react';
+import HomepageBody2 from '../layouts/HomepageBody2';
+
+
 const HomePage = () => {
     return(
         <>
             <div className="flex items-center justify-center min-h-screen">
-                <p className="text-3xl font-bold underline">I am the Home page, Welcome to Elevate Cv</p>
+                <p className="text-3xl font-bold underline">I am the Home page,Welcome to Elevate Cv</p>
             </div>
+         
+            <HomepageBody2/>
         </>
+
     )
+
 }
 
 export default HomePage
