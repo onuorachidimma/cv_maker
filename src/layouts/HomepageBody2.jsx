bcecf2ade10e73a46c550e5cec81a112af582edc
khadijah <phunmilawal@gmail.com>
2024-06-29 17:35:09 +0100

    Feat: Revamp tailwind

--- src/layouts/HomepageBody2.jsx
+++ src/layouts/HomepageBody2.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const HomepageBody2 = () => {
   return (
     <>
-      <div className="container">
-        <div className="section"><h1>Section 1</h1></div>
-        <div className="section"><h1>Section 2</h1></div>
-        <div className="section">Section 3</div>
+      <div className="container lg:grid-cols-3 bg-green-200 h-70 flex items-center justify-center p-10">
+        <div className="card h-40 bg-white p-4 m-10 rounded-lg w-full"><h1>Section 1</h1></div>
+        <div className="card h-40 bg-white p-4 m-10 rounded-lg w-full"><h1>Section 2</h1></div>
+        <div className="card h-40 bg-white p-4 m-10 rounded-lg w-full"><h1>Section 3</h1></div>
       </div>
     </>
   );
