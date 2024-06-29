a829aae4cce1e1ceff6f46ec3628a07d83065453
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-29 02:45:26 +0100

    feat: configure tailwind for

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,8 +1,16 @@
 const LoginPage = () => {
-    return(
-        <div>
-            LoginPAge
+    return (
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-2/3 bg-black text-white">
+          I am one
         </div>
-    )
-}
-export default LoginPage
+        <div className="w-full md:w-1/3 bg-purple text-white">
+          I am two
+        </div>
+        LoginPage
+      </div>
+    );
+  };
+  
+  export default LoginPage;
+  
