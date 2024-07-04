2dfd953ebe8f32142d9afa1a7838d22297a548cd
Anob <anobtakon@gmail.com>
2024-07-04 14:45:58 +0100

    made some adjustments to the signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
-import "./App.css";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import Signup from "./pages/Signup";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 export default App;
