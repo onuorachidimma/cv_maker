62923916e6a4eef5aa2a58773a47d0cf97cec434
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-07-04 01:10:25 +0100

    feat: create signUoForFree button

---
+++ src/layouts/buttons/signupFreeBtn.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+const SignupFreeButton = () => {
+    return(
+        <div>
+            <button className="bg-customYellow py-2.5 px-5 rounded-35px "><Link to="#">Sign Up - It’s completely free</Link></button>
+        </div>
+    )
+}
+export default SignupFreeButton
