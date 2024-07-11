490dda82870b3d59d407671a58a82d012b268868
khadijah <phunmilawal@gmail.com>
2024-07-11 17:58:09 +0100

    Feat: Changes in template

--- src/layouts/homepageBody2/HomepageBody2.jsx
+++ src/layouts/homepageBody2/HomepageBody2.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import smileyman from '../../assets/images/smileyman.png';
 
 const HomepageBody2 = () => {
   return (
     <>
       
 
-      <div class="bg-customYellow p-4 flex space-x-4 items-center justify-center mt-20 h-80">
-        <div class="bg-customYellowB px-4 w-1/4 h-60">
-          
+      <div class="bg-customYellow p-4 flex space-x-4 items-center justify-center mt-20 h-96">
+        <div className="bg-customYellowB  w-1/4 h-60">
+          <img src={smileyman} alt="Smiley Man" style={{ objectFit: 'contain' }} />
         </div>
-        <div class= "bg-customYellowB px-4 w-1/8 h-40 flex items-center justify-center ">
-          <h1 class="text-4xl font-bold mb-2 text-left max-w-md mx-auto ">Features Tailored for Your Success</h1>
+        <div class= "bg-customYellowB px-4 w-1/8 h-40 flex justify-center items-center">
+          <h1 class=" bg-red-500 text-5xl font-bold mb-2 text-left max-w-md mx-auto flex items-center justify-center ">Features Tailored for Your Success</h1>
         </div>
       </div>
 
