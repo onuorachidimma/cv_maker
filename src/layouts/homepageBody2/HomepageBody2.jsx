29ddff7d4cf6fb07895c7df81c7f97029db6bde1
khadijah <phunmilawal@gmail.com>
2024-07-12 21:26:27 +0100

    Feat: responsiveness updated

--- src/layouts/homepageBody2/HomepageBody2.jsx
+++ src/layouts/homepageBody2/HomepageBody2.jsx
@@ -5,12 +5,12 @@ const HomepageBody2 = () => {
   return (
     <>
 
-      <div class="bg-customYellow flex lg:flex-row items-center justify-center h-96 pb-10 sm:flex-col md:flex-col ">
-        <div className="lg:w-1/4 lg:h-60 sm:flex-col sm:flex ">
+      <div class="bg-customYellow lg:flex lg:flex-row items-center justify-center h-96 pb-10 sm:flex sm:flex-col sm:justify-center sm:items-center">
+        <div className="lg:w-1/4 lg:h-60 sm:flex-col sm:flex">
           <img src={smileyman} alt="Smiley Man" style={{ objectFit: 'contain' }} />
         </div>
-        <div class= "bg-customYellowB px-2 lg:w-1/8 lg:h-44 flex lg:flex-row justify-center items-center mt-12 sm:flex-col sm:h-auto">
-          <h1 class="text-5xl font-bold max-w-md mx-auto flex text-center sm:flex-col">Features Tailored for Your Success</h1>
+        <div class= "bg-customYellowB px-2 lg:w-1/8 lg:h-44 flex lg:flex-row justify-center items-center lg:mt-12 sm:flex-col">
+          <h1 class="lg:text-5xl font-bold max-w-md mx-auto flex text-center sm:flex-col text-3xl  ">Features Tailored for Your Success</h1>
         </div>
       </div>
 
