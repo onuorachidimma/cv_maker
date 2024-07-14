68f30d2f90b0f24c8c24017dc8f56d9d2d093c59
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-07-14 18:02:50 +0300

    "Feat: Finish the navbar and hero section"

--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -7,12 +7,12 @@ function Hero() {
         <h1 className="text-center text-6xl font-bold m-6">Craft Your Future with a CV That Captivates and Inspires.</h1>
         <h4 className="text-center text-xl m-10 mx-36 ">Our platform empowers you to create a captivating online CV that highlights your achievements, skills, and unique personality.</h4>
         <div className="flex m-10 justify-center gap-6"> 
-          <button className="bg-yellow-500 py-2.5 px-5 rounded-3xl font-semibold text-xl">Sign Up - It’s completely free</button>
-          <button className="bg-green-700 py-2.5 px-5 rounded-3xl font-semibold text-xl">Watch a Demo</button>
+          <button className="bg-customGreen py-2.5 px-5 rounded-3xl text-base">Create your CV</button>
+          <button className=" border border-customGreen py-2.5 px-5 rounded-3xl text-base">Upgrade your CV</button>
         </div>
-        <div className="flex m-10 justify-center gap-3 w-9/12 h-1/3">
-          <div className=" container flex-1 w-8 bg-green-700 rounded-tl-3xl rounded-br-3xl"></div>
-          <div className="container flex w-32 bg-green-200"></div>
+        <div className=" container flex justify-center flex-row gap-2">
+          <div className="container bg-customGreen w-5 h-auto rounded-tl-3xl rounded-br-3xl"></div>
+          <img src="src\assets\images\HeroImage.png" alt="custom CV image" />
         </div>
       </div>
     </>
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -8,11 +8,7 @@ const HomePage = () => {
         <>
             <HomepageNavbar />
             <Hero />
-            {/* <div className="flex items-center justify-center min-h-screen">
-                <p className="text-3xl font-bold underline">I am the Home page,Welcome to Elevate Cv</p>
-            </div> */}
-         
-            {/* <HomepageBody2/> */}
+            <HomepageBody1 />
         </>
 
     )
