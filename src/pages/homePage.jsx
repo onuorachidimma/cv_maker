fdf80c439a24fd66ffc3c07527435208197e236a
choquann <queenasuquo081@yahoo.com>
2024-07-15 12:49:01 +0100

    feat: footer updated

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import HomepageBody2 from '../layouts/homepageBody2/HomepageBody2';
 import HomepageBody1 from '../layouts/homepageBody1/homepageBody1';
 import CvTemplate from '../layouts/homepageBody2/cvTemplate';
+import Footer from '../layouts/footer/footer';
 
 const HomePage = () => {
     return(
@@ -13,6 +14,7 @@ const HomePage = () => {
             <HomepageBody1 />
             <HomepageBody2/>
             <CvTemplate/>
+            <Footer/>
         </>
 
     )
