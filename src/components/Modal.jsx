e8c6e8e9f9c9077aed2b92eb0dfa4e5e6eb41848
Anob <anobtakon@gmail.com>
2024-07-22 16:56:20 +0100

    add the link to login on the sign up page

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,70 +1,40 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import PropTypes from "prop-types";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import Vector_icon from "../assets/images/Vector-icon.svg";
 
-function Modal({ email, closeModal }) {
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+const Modal = ({ email, closeModal }) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
-  const [errors, setErrors] = useState({});
-  const [setPasswordCriteria] = useState({
-    length: false,
-    capital: false,
-    lowercase: false,
-    number: false,
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: email, // Use the email passed from Signup
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("First Name is required"),
+      lastName: Yup.string().required("Last Name is required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+      password: Yup.string()
+        .min(8, "Password must be at least 8 characters long")
+        .required("Password is required"),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Passwords must match")
+        .required("Confirm Password is required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      closeModal();
+    },
   });
-
-  const validateEmail = (email) => {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  };
-
-  const validatePassword = (password) => {
-    const length = password.length >= 8;
-    const capital = /[A-Z]/.test(password);
-    const lowercase = /[a-z]/.test(password);
-    const number = /[0-9]/.test(password);
-
-    setPasswordCriteria({
-      length,
-      capital,
-      lowercase,
-      number,
-    });
-
-    return length && capital && lowercase && number;
-  };
-
-  const handlePasswordChange = (e) => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
-    validatePassword(newPassword);
-  };
-
-  const handleSignUp = () => {
-    let tempErrors = {};
-    if (!email) tempErrors.email = "Email is required";
-    else if (!validateEmail(email)) tempErrors.email = "Email is invalid";
-
-    if (!password) tempErrors.password = "Password is required";
-    else if (!validatePassword(password))
-      tempErrors.password = "Password does not meet the criteria";
-
-    if (confirmPassword !== password)
-      tempErrors.confirmPassword = "Passwords do not match";
-
-    if (Object.keys(tempErrors).length === 0) {
-      if (email === "test@example.com") {
-        tempErrors.email = "Email is already registered";
-      } else {
-        closeModal();
-      }
-    }
-
-    setErrors(tempErrors);
-  };
 
   return (
     <div className="text-20px font-sans fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -77,75 +47,90 @@ function Modal({ email, closeModal }) {
           <img src={Vector_icon} alt="vector-icon" />
         </button>
         <p className="font-bold text-2xl text-center mt-7 pb-5">Sign Up</p>
-        <div className="mb-4">
-          <p className="pb-1 pl-1">Email</p>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email} // Display the email prop
-            readOnly
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-xs mt-1">{errors.email}</p>
-          )}
-        </div>
-        <div className="mb-4 relative">
-          <p className="pb-1 pl-1">Password</p>
-          <div className="relative flex items-center">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-4">
+            <p className="pb-1 pl-1">Email</p>
             <input
-              type={passwordVisible ? "text" : "password"}
-              placeholder="Password"
-              value={password}
-              onChange={handlePasswordChange}
+              name="email"
+              type="email"
+              placeholder="Email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              readOnly
             />
-            <button
-              type="button"
-              onClick={() => setPasswordVisible(!passwordVisible)}
-              className="absolute right-0 pr-3 text-gray-600"
-            >
-              {passwordVisible ? <FaEyeSlash /> : <FaEye />}
-            </button>
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-xs mt-1">
+                {formik.errors.email}
+              </div>
+            ) : null}
           </div>
-          {errors.password && (
-            <p className="text-red-500 text-xs mt-1">{errors.password}</p>
-          )}
-        </div>
-        <div className="mb-6 relative">
-          <p className="pb-1 pl-1">Confirm Password</p>
-          <div className="relative flex items-center">
-            <input
-              type={confirmPasswordVisible ? "text" : "password"}
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <button
-              type="button"
-              onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
-              className="absolute right-0 pr-3 text-gray-600"
-            >
-              {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
-            </button>
+          <div className="mb-4 relative">
+            <p className="pb-1 pl-1">Password</p>
+            <div className="relative flex items-center">
+              <input
+                name="password"
+                type={passwordVisible ? "text" : "password"}
+                placeholder="Password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <button
+                type="button"
+                onClick={() => setPasswordVisible(!passwordVisible)}
+                className="absolute right-0 pr-3 text-gray-600"
+              >
+                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-500 text-xs mt-1">
+                {formik.errors.password}
+              </div>
+            ) : null}
           </div>
-          {errors.confirmPassword && (
-            <p className="text-orange-700 text-xs mt-1">
-              {errors.confirmPassword}
-            </p>
-          )}
-        </div>
-        <button
-          onClick={handleSignUp}
-          className="w-80 rounded-3xl sm:w-auto sm:rounded-xl font-bold py-2 bg-customYellow shadow-lg hover:bg-customYellow transition duration-300 text-md mx-auto block sm:px-11 sm:py-2"
-        >
-          Sign Up
-        </button>
+          <div className="mb-6 relative">
+            <p className="pb-1 pl-1">Confirm Password</p>
+            <div className="relative flex items-center">
+              <input
+                name="confirmPassword"
+                type={confirmPasswordVisible ? "text" : "password"}
+                placeholder="Confirm Password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.confirmPassword}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <button
+                type="button"
+                onClick={() =>
+                  setConfirmPasswordVisible(!confirmPasswordVisible)
+                }
+                className="absolute right-0 pr-3 text-gray-600"
+              >
+                {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+              <div className="text-red-500 text-xs mt-1">
+                {formik.errors.confirmPassword}
+              </div>
+            ) : null}
+          </div>
+          <button
+            type="submit"
+            className="px-8 font-bold block py-2 mx-auto bg-customYellow rounded-xl hover:bg-yellow-300 transition duration-200"
+          >
+            Sign up
+          </button>
+        </form>
       </div>
     </div>
   );
-}
+};
 
 Modal.propTypes = {
   email: PropTypes.string.isRequired,
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,26 +2,20 @@ import { useState } from "react";
 import Logo from "../assets/images/Logo-whitebg.svg";
 import Socialsbtn from "../components/SocialAuthButtons";
 import Modal from "../components/Modal";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [email, setEmail] = useState(""); // Add state for email
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
 
-  // Arrow function to open the modal
+  // Function to open the modal
   const openModal = () => {
     setIsModalOpen(true);
   };
 
-  // Arrow function to close the modal
+  // Function to close the modal
   const closeModal = () => {
     setIsModalOpen(false);
-  };
-
-  // Arrow function for navigation
-  const handleLoginClick = () => {
-    navigate("/loginPage");
   };
 
   return (
@@ -30,12 +24,12 @@ const Signup = () => {
         <div className="h-full flex flex-col">
           <div className="flex justify-between items-center">
             <img className="h-24 w-96" src={Logo} alt="Logo" />
-            <button
-              onClick={handleLoginClick} // Use handleLoginClick for navigation
+            <Link
+              to="/login"
               className="rounded-lg border border-black bg-green-100 px-7 py-2 text-sm"
             >
               Login
-            </button>
+            </Link>
           </div>
           <div className="flex flex-col items-center justify-center h-full">
             <div className="flex flex-col items-center justify-center text-center gap-2">
@@ -51,14 +45,14 @@ const Signup = () => {
             </div>
 
             <div className="flex flex-col items-center justify-center">
-              <p className="mb-5 text-lg  border-customLight">
+              <p className="mb-5 text-lg border-customLight">
                 Or sign up with your Email
               </p>
               <input
                 placeholder="Email"
-                value={email} // Bind email state to the input
-                onChange={(e) => setEmail(e.target.value)} // Handle input change
-                className="lg:max-w-lg rounded border border-gray-300 py-5 pl-4 pr-48"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="rounded border border-gray-300 py-5 pl-4 px-24"
               />
               <button
                 onClick={openModal}
@@ -71,17 +65,16 @@ const Signup = () => {
         </div>
 
         <div className="flex items-center justify-center mt-5">
-          <p className="text-xs md:text-sm  border-customLight text-center">
+          <p className="text-xs md:text-sm border-customLight text-center">
             By signing up, I agree to the
-            <span className="text-[#005148]"> Terms of Use </span>
+            <span className="customDeepGreen]"> Terms of Use </span>
             and
-            <span className="text-[#005148]"> Privacy Policy</span>.
+            <span className="customDeepGreen"> Privacy Policy</span>.
           </p>
         </div>
       </div>
       <div className="hidden lg:block lg:w-[10%] md:bg-customDarkGreen lg:bg-customDarkGreen" />
-      {isModalOpen && <Modal email={email} closeModal={closeModal} />}{" "}
-      {/* Pass email to Modal */}
+      {isModalOpen && <Modal email={email} closeModal={closeModal} />}
     </div>
   );
 };
