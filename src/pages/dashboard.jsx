0cf381f8b7a067e26f4b42863f044cf86c9575e3
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-07-29 10:42:28 +0100

    feat: create dashboard and custom loader

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold">Welcome to ElevateCv, your dream job is one click away!</h1>
+      </div>
+    );
+  };
+  
+  export default Dashboard;
