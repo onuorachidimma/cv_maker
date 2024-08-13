a8bb2d080c3e55f14d646f88583afd374fa30c98
Anob <anobtakon@gmail.com>
2024-08-13 23:55:15 +0100

    add the right image path

--- src/pages/templates.jsx
+++ src/pages/templates.jsx
@@ -1,5 +1,9 @@
 import Footer from "../layouts/footer/footer";
-import template from "../assets/images/reviewerImage1.svg";
+import template1 from "../assets/images/template1.svg";
+import template2 from "../assets/images/template2.svg";
+import template3 from "../assets/images/template3.svg";
+import template4 from "../assets/images/template4.svg";
+import template5 from "../assets/images/template5.svg";
 
 function Templates() {
   return (
@@ -10,13 +14,12 @@ function Templates() {
           You can always change your mind and try a different template later.
         </p>
       </div>
-      <div>
-        <img src={template} />
-        <img src={template} />
-        <img src={template} />
-        <img src={template} />
-        <img src={template} />
-
+      <div className="flex">
+        <img src={template1} />
+        <img src={template2} />
+        <img src={template3} />
+        <img src={template4} />
+        <img src={template5} />
       </div>
       <Footer />
     </div>
