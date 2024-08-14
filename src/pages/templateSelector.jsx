13b88c67a8dd33e15ef5a9c508127b18109bf53d
Anob <anobtakon@gmail.com>
2024-08-14 02:44:49 +0100

    restrict the width of see template

--- src/pages/templateSelector.jsx
+++ src/pages/templateSelector.jsx
@@ -27,7 +27,7 @@ function TemplateSelector() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex flex-col justify-center items-center flex-grow">
+      <div className="flex flex-col  flex-grow px-64">
         <div className="text-center mb-4">
           <h2 className="font-bold text-6xl mb-4 mt-9">
             Choose a template for your CV
@@ -36,52 +36,55 @@ function TemplateSelector() {
             You can always change your mind and try a different template later
           </p>
         </div>
-        <div className="flex justify-center items-center">
-          <img
-            id="template1"
-            className={getImageClasses("template1")}
-            src={template1}
-            alt="Template 1"
-            onClick={() => handleTemplateClick("template1")}
-          />
-          <img
-            id="template2"
-            className={getImageClasses("template2")}
-            src={template2}
-            alt="Template 2"
-            onClick={() => handleTemplateClick("template2")}
-          />
-          <img
-            id="template3"
-            className={getImageClasses("template3")}
-            src={template3}
-            alt="Template 3"
-            onClick={() => handleTemplateClick("template3")}
-          />
-          <img
-            id="template4"
-            className={getImageClasses("template4")}
-            src={template4}
-            alt="Template 4"
-            onClick={() => handleTemplateClick("template4")}
-          />
-          <img
-            id="template5"
-            className={getImageClasses("template5")}
-            src={template5}
-            alt="Template 5"
-            onClick={() => handleTemplateClick("template5")}
-          />
+        <div className="flex flex-col">
+          <div className="flex">
+            <img
+              id="template1"
+              className={getImageClasses("template1")}
+              src={template1}
+              alt="Template 1"
+              onClick={() => handleTemplateClick("template1")}
+            />
+            <img
+              id="template2"
+              className={getImageClasses("template2")}
+              src={template2}
+              alt="Template 2"
+              onClick={() => handleTemplateClick("template2")}
+            />
+            <img
+              id="template3"
+              className={getImageClasses("template3")}
+              src={template3}
+              alt="Template 3"
+              onClick={() => handleTemplateClick("template3")}
+            />
+            <img
+              id="template4"
+              className={getImageClasses("template4")}
+              src={template4}
+              alt="Template 4"
+              onClick={() => handleTemplateClick("template4")}
+            />
+            <img
+              id="template5"
+              className={getImageClasses("template5")}
+              src={template5}
+              alt="Template 5"
+              onClick={() => handleTemplateClick("template5")}
+            />
+          </div>
+          <Link
+            to="/templates"
+            className=" underline underline-offset-2 cursor-pointer text-right"
+          >
+            See all templates
+          </Link>
         </div>
-        <Link
-          to="/templates"
-          className="mt-4 underline underline-offset-2 cursor-pointe text-right"
-        >
-          See all templates
-        </Link>
-        <div className="flex mr-7">
-          <p className="text-customLighterGreen py-2">Skip for now</p>
-          <button className="text-white bg-customDeepGreen text-xl font-light py-3 px-">
+
+        <div className="flex justify-end gap-7">
+          <p className="text-customLighterGreen py-2 my-5">Skip for now</p>
+          <button className="text-white bg-customDeepGreen text-xl font-light py-3 px-7 my-5">
             Choose the template
           </button>
         </div>
