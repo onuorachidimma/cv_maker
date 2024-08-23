123063f1ba4d9430fbcce194dba5b2c2da26da44
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-08-23 18:31:00 +0100

    .

--- src/pages/cvRankerPage.jsx
+++ src/pages/cvRankerPage.jsx
@@ -1,15 +1,14 @@
-import HomepageNavbar from "../layouts/HomepageNavbar"
-import SectionHeading from "../layouts/homepageBody1/sectionHeading"
+import HomepageNavbar from "../layouts/HomepageNavbar";
+import SectionHeading from "../layouts/homepageBody1/sectionHeading";
 const CVRanker = () => {
-    return(
-        
-        <div>
-            <HomepageNavbar />
-            <div className="mt-20 ml-20">
-                <SectionHeading heading="CV Ranker" />
-                <p className="font-semibold">Job Description</p>
-            </div>
-        </div>
-    )
-}
-export default CVRanker
+  return (
+    <div>
+      <HomepageNavbar />
+      <div className="mt-20 ml-20">
+        <SectionHeading heading="CV Ranker" />
+        <p className="font-semibold">Job Description</p>
+      </div>
+    </div>
+  );
+};
+export default CVRanker;
