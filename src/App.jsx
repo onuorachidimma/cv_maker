4cec53a955738bcb459882c5e2507404d15a63d2
khadijah <phunmilawal@gmail.com>
2024-09-18 12:08:52 +0100

    feat: subscription page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./pages/signup";
 import CVRanker from "./pages/cvRankerPage";
 import Dashboard from "./pages/dashboard";
 import PrivateRoute from "./components/privateRoutes";
+import SubscriptionPage from "./pages/subscriptionPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/rankCV" element={<CVRanker />} />
+      <Route path="/subscription" element={<SubscriptionPage />} />
       <Route
         path="/dashboard"
         element={
