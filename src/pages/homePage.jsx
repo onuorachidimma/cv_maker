42a9e0999f98aa81f004198e989f0c3b8f086649
Queen <you@example.com>
2024-09-30 13:34:26 +0100

    feat: updated the homepage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,9 +3,7 @@ import HomepageNavbar from '../layouts/HomepageNavbar'
 import React from 'react';
 import HomepageBody2 from '../layouts/homepageBody2/HomepageBody2';
 import HomepageBody1 from '../layouts/homepageBody1/homepageBody1';
-import CvTemplate from '../layouts/homepageBody2/cvTemplate';
-import BuildCV from '../layouts/buildCV/buildCV.jsx';
-// import Footer from '../layouts/footer/footer';
+import JordanCV from '../layouts/jordanCV/jordanCV.jsx';
 import Footer from '../layouts/footer/footer.jsx';
 
 const HomePage = () => {
@@ -15,8 +13,8 @@ const HomePage = () => {
             <Hero />
             <CvTemplate/>
             <HomepageBody1 />
-            {/* <HomepageBody2/> */}
-            <BuildCV />
+            <HomepageBody2/>
+            <JordanCV /> 
             <Footer />
         </>
 
